fb1fc04876077186d129a40511ca42d576c8d885
arthosd <elie.debs@outlook.fr>
2022-01-06 14:57:40 +0100

    Adding front end to extract aspect from any entered string

--- static/backend.js
+++ static/backend.js
@@ -1,10 +1,15 @@
 function analyze () {
 
     const message = document.getElementById('enter').value;
+    const button = document.getElementById('button')
+
+    button.disabled = true;
 
     // Check if text is empty   
     if (message == "") {
-        return undefined
+
+        button.disabled = false;
+        return undefined;
     }
 
     var inputs_obj = {
@@ -19,7 +24,30 @@ function analyze () {
         data :JSON.stringify(inputs_obj),
         encode : true,
         success : function (data) {
-            console.log(data)
+
+            data_to_map = data.data;
+
+            const button = document.getElementById('button')
+            const div = document.getElementById("placeholder")
+            var html_to_append = ""
+
+            console.log(data_to_map.length)
+
+            if (data_to_map.length == 0) { // If there's no response
+
+                button.disabled = false;
+                div.innerHTML = " No aspect found !"
+
+                return undefined;
+            }
+
+            data_to_map.map(function (data) {
+
+                html_to_append = html_to_append + "<p>"+data.aspect.toString()+ "</p>"
+            })
+
+            div.innerHTML = html_to_append
+            button.disabled = false;
         }
     })
 }
