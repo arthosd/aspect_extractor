2cca0b80ecad7b73193ca50bd5e056b9f9b7f522
arthosd <elie.debs@outlook.fr>
2022-01-07 23:25:27 +0100

    Adding Pattern

--- app/nlp/pattern.py
+++ app/nlp/pattern.py
@@ -43,8 +43,31 @@ rules = {
             "RIGHT_ID": "subject",
             "RIGHT_ATTRS": {"DEP": "acomp"}
         }
-    ]
-
+    ],
+    "rule_conj" : [
+        {
+            "RIGHT_ID": "aux",
+            "RIGHT_ATTRS": {"POS": "AUX"}
+        },
+        {
+            "LEFT_ID": "aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "expression",
+            "RIGHT_ATTRS": {"DEP": "nsubj", "POS" : "NOUN"}
+        },
+        {
+            "LEFT_ID": "aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "subject",
+            "RIGHT_ATTRS": {"DEP": "acomp"}
+        },
+        {
+            "LEFT_ID": "subject",
+            "REL_OP": ">",
+            "RIGHT_ID": "conj",
+            "RIGHT_ATTRS": {"DEP": "conj"}
+        }
+    ],
 }
 
 def init_patterns (dep_patcher) :
